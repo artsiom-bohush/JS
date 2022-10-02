cbb1e01ae5249b6f148ea7f47fd9b9a83afd3994
Artsiom Bohush <artyom.bogush@gmail.com>
2022-10-02 10:41:42 +0200

    add Lesson2

---
+++ Lesson2/script/script.js
@@ -0,0 +1,16 @@
+
+
+const a = prompt('первое число');
+
+const b = prompt('второе число');
+
+
+
+if (a > b){
+    console.log(+a - +b);
+    }else if (a < b){
+        console.log(+b - +a);
+    }
+
+    
+
